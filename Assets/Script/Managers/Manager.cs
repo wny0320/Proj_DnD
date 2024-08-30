fb25ec5079c8df3856d587cae3ed90ee327fa78c
허지웅 <joe0617160@naver.com>
2024-08-30 15:56:25 +0900

    add loading

--- Assets/Script/Managers/Manager.cs
+++ Assets/Script/Managers/Manager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
@@ -64,8 +66,90 @@ public class Manager : MonoBehaviour
     public void StartInvenCoroutine()
     {
         if (invenCoroutine == null)
+        {
             invenCoroutine = StartCoroutine(Inven.MoveItem());
+        }
         else
             return;
     }
+
+
+
+    #region Scene이동관련
+
+    [SerializeField]
+    private GameObject loadingSceneUI;
+    private CanvasGroup sceneLoaderCanvasGroup;
+    private Image progressBar;
+
+    private string loadSceneName;
+    public void LoadScene(string sceneName)
+    {
+        sceneLoaderCanvasGroup = Instantiate(loadingSceneUI).GetComponent<CanvasGroup>();
+        progressBar = sceneLoaderCanvasGroup.transform.GetChild(1).GetComponent<Image>();
+
+        SceneManager.sceneLoaded += LoadSceneEnd;
+        loadSceneName = sceneName;
+        StartCoroutine(Load(sceneName));
+    }
+
+    private IEnumerator Load(string sceneName)
+    {
+        progressBar.fillAmount = 0f;
+        yield return StartCoroutine(Fade(true));
+
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);
+        op.allowSceneActivation = false;
+
+        float timer = 0.0f;
+        while (!op.isDone)
+        {
+            yield return null;
+            timer += Time.unscaledDeltaTime;
+
+            if (op.progress < 0.9f)
+            {
+                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, op.progress, timer);
+                if (progressBar.fillAmount >= op.progress)
+                {
+                    timer = 0f;
+                }
+            }
+            else
+            {
+                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 1f, timer);
+
+                if (progressBar.fillAmount == 1.0f)
+                {
+                    op.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+        }
+    }
+
+    private void LoadSceneEnd(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (scene.name == loadSceneName)
+        {
+            StartCoroutine(Fade(false));
+            SceneManager.sceneLoaded -= LoadSceneEnd;
+        }
+    }
+
+    private IEnumerator Fade(bool isFadeIn)
+    {
+        float timer = 0f;
+
+        while (timer <= 1f)
+        {
+            yield return null;
+            timer += Time.unscaledDeltaTime * 2f;
+            sceneLoaderCanvasGroup.alpha = Mathf.Lerp(isFadeIn ? 0 : 1, isFadeIn ? 1 : 0, timer);
+        }
+
+        if (!isFadeIn)
+            Destroy(sceneLoaderCanvasGroup.gameObject);
+    }
+    #endregion
 }
