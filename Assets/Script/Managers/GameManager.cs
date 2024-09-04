ba3adb9a7dbf26d0c6abbf4b6deea571e11e98fe
허지웅 <joe0617160@naver.com>
2024-09-04 16:19:03 +0900

    minimap

--- Assets/Script/Managers/GameManager.cs
+++ Assets/Script/Managers/GameManager.cs
@@ -7,9 +7,20 @@ public class GameManager
     // 게임 매니저 - 게임 흐름 관련 작성
     public GameObject Player;
     public bool isPlayerAlive = true;
+    public bool isCursorLock = true;
 
     public void OnAwake()
     {
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void CursorLock(bool cursurLock)
+    {
+        isCursorLock = cursurLock;
+
+        if(cursurLock)
+            Cursor.lockState = CursorLockMode.Locked;
+        else
+            Cursor.lockState = CursorLockMode.None;
     }
 }
